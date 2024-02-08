3c7d706ebf661b9a14a6f70a0d31e654eddd899b
Philip Johnson <p.zhao.johnson@gmail.com>
2024-02-07 21:18:59 -0500

    Maintain first and last base coordinates instead of centroid

--- src/flipSelfX.test.js
+++ src/flipSelfX.test.js
@@ -2,8 +2,6 @@ import { flipSelfX } from './flipSelfX';
 
 import { NucleobaseMock } from './NucleobaseMock';
 
-import { Centroid } from './Centroid';
-
 describe('flipSelfX function', () => {
   test('zero target bases', () => {
     let targetBases = [];
@@ -52,11 +50,11 @@ describe('flipSelfX function', () => {
     flipSelfX(targetBases);
 
     let expectedCoordinates = [
-      { x: 9.22360549717057, y: 29.48860145513339 },
-      { x: 76.38447857720291, y: -11.674696847210996 },
-      { x: -8.866936135812455, y: 41.05286984640259 },
-      { x: -29.964753435731605, y: 33.64462409054163 },
-      { x: 77.22360549717058, y: 47.4886014551334 },
+      { x: 12, y: 19 },
+      { x: 79.16087308003235, y: -22.163298302344387 },
+      { x: -6.090541632983026, y: 30.564268391269195 },
+      { x: -27.188358932902176, y: 23.15602263540824 },
+      { x: 80.00000000000001, y: 37.00000000000001 },
     ];
 
     expectedCoordinates.forEach((cs, i) => {
@@ -77,11 +75,11 @@ describe('flipSelfX function', () => {
     flipSelfX(targetBases);
 
     let expectedCoordinates = [
-      { x: 998.8656780487805, y: 187.50950243902454 },
-      { x: 838.0007024390244, y: 168.13272195121965 },
-      { x: 54.58743414634159, y: -216.61869268292696 },
-      { x: 4.68050731707325, y: -10.533034146341635 },
-      { x: 291.86567804878064, y: 138.50950243902435 },
+      { x: 1012, y: -2 },
+      { x: 851.1350243902439, y: -21.376780487804893 },
+      { x: 67.72175609756107, y: -406.12819512195153 },
+      { x: 17.814829268292726, y: -200.04253658536618 },
+      { x: 305.0000000000001, y: -51.0000000000002 },
     ];
 
     expectedCoordinates.forEach((cs, i) => {
@@ -90,7 +88,7 @@ describe('flipSelfX function', () => {
     });
   });
 
-  it('maintains centroid', () => {
+  it('maintains the coordinates of the first and last bases', () => {
     let targetBases = [
       new NucleobaseMock({ centerPoint: { x: 1, y: 12 } }),
       new NucleobaseMock({ centerPoint: { x: 22, y: 9 } }),
@@ -98,15 +96,16 @@ describe('flipSelfX function', () => {
       new NucleobaseMock({ centerPoint: { x: 18, y: -12 } }),
     ];
 
-    let centroid = new Centroid(targetBases);
-
-    expect(centroid.get().x).toBeCloseTo(19.5);
-    expect(centroid.get().y).toBeCloseTo(0.25);
-
     flipSelfX(targetBases);
 
-    // was maintained
-    expect(centroid.get().x).toBeCloseTo(19.5);
-    expect(centroid.get().y).toBeCloseTo(0.25);
+    // were maintained
+    expect(targetBases[0].getCenterPoint().x).toBeCloseTo(1);
+    expect(targetBases[0].getCenterPoint().y).toBeCloseTo(12);
+
+    // were maintained
+    expect(targetBases[3].getCenterPoint().x).toBeCloseTo(18);
+    expect(targetBases[3].getCenterPoint().y).toBeCloseTo(-12);
+
+    expect(targetBases.length).toBe(4);
   });
 });
--- src/flipSelfX.ts
+++ src/flipSelfX.ts
@@ -1,25 +1,30 @@
 import type { Nucleobase } from './Nucleobase';
 
-import { Centroid } from './Centroid';
-
 import { Direction } from './Direction';
 
 import { rotate } from './rotate';
 
 import { flipX } from './flipX';
 
+import { shift } from './shift';
+
+import { displacement } from '@rnacanvas/points';
+
 /**
- * Flips the target bases across a line passing through the centroid of the target bases
- * and having a direction equal to the overall direction of the target bases.
+ * Flips the target bases across the line passing from the first to the last of the target bases.
  */
 export function flipSelfX(targetBases: Nucleobase[]): void {
-  let centroid = new Centroid(targetBases);
+  if (targetBases.length == 0) {
+    // nothing to do
+    return;
+  }
 
-  let { x, y } = centroid.get();
+  let firstBase = targetBases[0];
 
-  // cache the actual numbers just in case the gotten centroid object has live properties
-  let originalCentroidX = x;
-  let originalCentroidY = y;
+  let { x, y } = firstBase.getCenterPoint();
+
+  // cache the actual numbers just in case the gotten object has live properties
+  let cachedFirstBaseCenterPoint = { x, y };
 
   // note that directly setting the direction of the target bases after flipping them can be tricky to think about
   let direction = new Direction(targetBases);
@@ -34,6 +39,7 @@ export function flipSelfX(targetBases: Nucleobase[]): void {
   // undo the rotation of the target bases
   rotate(targetBases, originalDirection);
 
-  // make sure centroid is maintained
-  centroid.set({ x: originalCentroidX, y: originalCentroidY });
+  // maintain the positioning of the first base
+  // (and effectively the last base as well)
+  shift(targetBases, displacement(firstBase.getCenterPoint(), cachedFirstBaseCenterPoint));
 }
